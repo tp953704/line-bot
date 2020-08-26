c409b426668b7baae8b3688893080ca5047bbe35
Wang <tp953704@gmail.com>
2020-08-26 20:45:15 +0800

    safe

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 
 // create LINE SDK config from env variables
-const config = {
-  channelAccessToken: 'hCqv0e3rPDct9qekXe9wTkWGGsjR0gmD3QtjjQisKyTVzo8KZkYfxz6KaA/ozjigj2exTzU9c1bcs5rhKrCCj36kwa0KQUzeZ92zRgfqgrl+DQmVZLFdk3Zxl7sJKZFGitVyE9NwHV/TXzpbmXoXSgdB04t89/1O/w1cDnyilFU=',
-  channelSecret: 'd1873a995298792d251377ced6ceb869',
-};
+const config = require('./config');
 
 // create LINE SDK client
 const client = new line.Client(config);
@@ -16,17 +13,19 @@ const client = new line.Client(config);
 // about Express itself: https://expressjs.com/
 const app = express();
 
+
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 console.log(12)
 app.post('/webhook', line.middleware(config), (req, res) => {
-    console.log(req)
+    req.body.events.map((item) => {
+      console.log(item);
+  })
   Promise
     .all(req.body.events.map((item) => {
         handleEvent(item);
     }))
     .then((result) => {
-        console.log(result);
         res.json(result);
     })
     .catch((err) => {
@@ -37,17 +36,135 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 // event handler
 function handleEvent(event) {
-    console.log(['EVENT',event])
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
+  // if (event.type !== 'message' || event.message.type !== 'text') {
+  //   // ignore non-text-message event
+  //   return Promise.resolve(null);
+  // }
 
   // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
-console.log(event.message.text);
+ 
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  
+  const appBubble = {
+    "type": "flex",
+    "altText": "This is a Flex Message",
+    "contents": {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": "https://raw.githubusercontent.com/tp953704/line-bot/master/assert/img/%E5%9C%96%E7%89%873.png",
+        "size": "full",
+        "aspectRatio": "20:20",
+        "aspectMode": "cover",
+        "action": {
+          "type": "uri",
+          "uri": "http://linecorp.com/"
+        },
+        "position": "relative",
+        "margin": "md",
+        "align": "center",
+        "gravity": "bottom"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "小帥哥一號",
+            "weight": "bold",
+            "size": "xl"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "margin": "lg",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Place",
+                    "color": "#aaaaaa",
+                    "size": "sm",
+                    "flex": 1
+                  },
+                  {
+                    "type": "text",
+                    "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                    "wrap": true,
+                    "color": "#666666",
+                    "size": "sm",
+                    "flex": 5
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Time",
+                    "color": "#aaaaaa",
+                    "size": "sm",
+                    "flex": 1
+                  },
+                  {
+                    "type": "text",
+                    "text": "10:00 - 23:00",
+                    "wrap": true,
+                    "color": "#666666",
+                    "size": "sm",
+                    "flex": 5
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "horizontal",
+        "spacing": "xs",
+        "contents": [
+          {
+            "type": "button",
+            "style": "link",
+            "height": "sm",
+            "action": {
+              "type": "uri",
+              "label": "WEBSITE",
+              "uri": "https://linecorp.com"
+            }
+          },
+          {
+            "type": "button",
+            "style": "link",
+            "height": "sm",
+            "action": {
+              "type": "uri",
+              "label": "WEBSITE",
+              "uri": "https://linecorp.com"
+            }
+          },
+          {
+            "type": "spacer",
+            "size": "sm"
+          }
+        ],
+        "flex": 1
+      }
+    }
+  };
+  return client.replyMessage(event.replyToken, appBubble);
 }
 
 // listen on port
