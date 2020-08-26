8624bf3178b19d7626d0e89bfe234e51cfe6f259
Wang <tp953704@gmail.com>
2020-08-26 20:48:58 +0800

    safesa

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
  module.exports = {
-    
+    channelAccessToken: 'hCqv0e3rPDct9qekXe9wTkWGGsjR0gmD3QtjjQisKyTVzo8KZkYfxz6KaA/ozjigj2exTzU9c1bcs5rhKrCCj36kwa0KQUzeZ92zRgfqgrl+DQmVZLFdk3Zxl7sJKZFGitVyE9NwHV/TXzpbmXoXSgdB04t89/1O/w1cDnyilFU=',
+    channelSecret: 'd1873a995298792d251377ced6ceb869',
   };
